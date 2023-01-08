4cab08baf60396533fb0dd59cd1a59855224e9e3
Abu Haniifa <35391609+abuhaniifa97@users.noreply.github.com>
2023-01-09 02:03:45 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>hCaptcha Demo</title>
+    <script src="https://js.hcaptcha.com/1/api.js" async defer></script>
+  </head>
+  <body>
+    <form action="" method="POST">
+      <input type="text" name="email" placeholder="Email" />
+      <input type="password" name="password" placeholder="Password" />
+      <div class="h-captcha" data-sitekey="c3d7cead-7423-4847-831d-5ca791d96053"></div>
+      <br />
+      <input type="submit" value="Submit" />
+    </form>
+  </body>
+</html>
